82625db03b629d3551c9991b3924a99efb2cfade
Stevie <xxlennygamerxx@gmail.com>
2021-07-18 16:24:03 -0700

    hi
    (cherry picked from commit 5c0bdaa25a05aa8df761a75da185ec5adbf88a42)

--- Funkin.NET/FunkinGame.cs
+++ Funkin.NET/FunkinGame.cs
@@ -12,12 +12,10 @@ using osu.Framework.Allocation;
 using osu.Framework.Configuration;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
-using osu.Framework.Graphics.Cursor;
 using osu.Framework.Graphics.Textures;
 using osu.Framework.IO.Stores;
 using osu.Framework.Platform;
 using osu.Framework.Screens;
-using osuTK;
 
 namespace Funkin.NET
 {
@@ -26,13 +24,8 @@ namespace Funkin.NET
     /// </summary>
     public class FunkinGame : Game, IBackgroundDependencyLoadable
     {
-        #region Constants
-
         public const string ProgramName = "Funkin.NET";
 
-        #endregion
-
-        #region Static Fields & Properties
 
         /// <summary>
         ///     Active <see cref="GameHost"/> instance. Resolved at runtime in <see cref="Main"/>.
@@ -55,10 +48,6 @@ namespace Funkin.NET
             @"Fonts/Funkin"
         };
 
-        #endregion
-
-        #region Instanced Fields & Properties
-
         public ScreenStack ScreenStack { get; private set; }
 
         public TextureStore TextureStore { get; private set; }
@@ -71,21 +60,15 @@ namespace Funkin.NET
 
         public string[] FunnyText { get; }
 
-        #endregion
-
-        #region Overridden Properties
 
         protected override Container<Drawable> Content { get; }
-
-        #endregion
-
-        #region Constructors
 
         public FunkinGame()
         {
             Name = ProgramName;
 
-            base.Content.Add(Content = new SafeAreaContainer
+            /*
+            Content = new SafeAreaContainer
             {
                 RelativeSizeAxes = Axes.Both,
 
@@ -98,17 +81,15 @@ namespace Funkin.NET
                         new SelectionKeyBindingContainer(this)
                     }
                 }
-            });
+            }
+             */
+            base.Content.Add(null);
 
             string path = Path.Combine("Json", "IntroText.json");
             string text = File.ReadAllText(path);
             FunnyTextList = JsonConvert.DeserializeObject<List<string[]>>(text);
             FunnyText = FunnyTextList?[new Random().Next(0, FunnyTextList.Count)];
         }
-
-        #endregion
-
-        #region Overridden Methods
 
         protected override void LoadComplete()
         {
@@ -120,9 +101,6 @@ namespace Funkin.NET
         protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent) =>
             DependencyContainer = new DependencyContainer(base.CreateChildDependencies(parent));
 
-        #endregion
-
-        #region BackgroundDependencyLoader
 
         [BackgroundDependencyLoader]
         void IBackgroundDependencyLoadable.BackgroundDependencyLoad()
@@ -139,7 +117,5 @@ namespace Funkin.NET
             Add(ScreenStack = new ScreenStack());
             ScreenStack.Push(IntroScreen = new IntroScreen());
         }
-
-        #endregion
     }
 }
