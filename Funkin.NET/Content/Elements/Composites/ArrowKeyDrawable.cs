6f183004824245037fc55b545008dbed7123d650
Chik3r <ikervaraalarcon@gmail.com>
2021-07-18 23:23:54 -0400

    load press and confirm anim

--- Funkin.NET/Content/Elements/Composites/ArrowKeyDrawable.cs
+++ Funkin.NET/Content/Elements/Composites/ArrowKeyDrawable.cs
@@ -2,6 +2,8 @@
 using Funkin.NET.Common.KeyBinds.ArrowKeys;
 using Funkin.NET.Core.BackgroundDependencyLoading;
 using osu.Framework.Allocation;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Animations;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Sprites;
 using osu.Framework.Graphics.Textures;
@@ -10,26 +12,65 @@ namespace Funkin.NET.Content.Elements.Composites
 {
     public class ArrowKeyDrawable : CompositeDrawable, IBackgroundDependencyLoadable
     {
+        // Notes:
+        // "x press" is for when you incorrectly press a key, or continue holding a key after key ends
+        // "x confirm" is for when you correctly press a key, and continue holding while the key hasn't ended
+        
         public ArrowKeyDrawable(ArrowKeyAction arrowKey)
         {
             ArrowKey = arrowKey;
-            ArrowSprite = new Sprite();
+            ArrowIdleSprite = new Sprite
+            {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre
+            };
+            ArrowPressAnim = new TextureAnimation
+            {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                IsPlaying = false,
+                Loop = false 
+            };
+            ArrowConfirmAnim = new TextureAnimation
+            {
+                Origin = Anchor.Centre,
+                Anchor = Anchor.Centre,
+                IsPlaying = false,
+                Loop = false
+            };
         }
 
         [Resolved] private TextureStore Textures { get; set; }
         public ArrowKeyAction ArrowKey { get; }
-        public Sprite ArrowSprite { get; }
+        public Sprite ArrowIdleSprite { get; }
+        public TextureAnimation ArrowPressAnim { get; }
+        public TextureAnimation ArrowConfirmAnim { get; }
+        
         
         [BackgroundDependencyLoader]
         void IBackgroundDependencyLoadable.BackgroundDependencyLoad()
         {
-            // Get the arrow texture
-            string arrowName = Enum.GetName(ArrowKey)?.ToUpperInvariant() ?? throw new ArgumentOutOfRangeException(nameof(ArrowKey));
-            string textureName = "Arrow/arrow" + arrowName;
-            ArrowSprite.Texture = Textures.Get(textureName);
+            string keyName = Enum.GetName(ArrowKey)?.ToUpperInvariant() ?? throw new ArgumentOutOfRangeException(nameof(ArrowKey));
             
-            // Add the texture
-            AddInternal(ArrowSprite);
+            // Get the arrow texture
+            string arrowName = keyName;
+            string textureName = "Arrow/arrow" + arrowName;
+            ArrowIdleSprite.Texture = Textures.Get(textureName);
+
+            keyName = keyName.ToLowerInvariant();
+            for (int i = 0; i < 4; i++)
+            {
+                ArrowPressAnim.AddFrame(Textures.Get($"Arrow/{keyName} press{i}"));
+                ArrowConfirmAnim.AddFrame(Textures.Get($"Arrow/{keyName} confirm{i}"));
+            }
+            
+            // Add the textures
+            AddInternal(ArrowIdleSprite);
+            AddInternal(ArrowPressAnim);
+            AddInternal(ArrowConfirmAnim);
+            
+            ArrowPressAnim.Hide();
+            ArrowConfirmAnim.Hide();
         }
     }
 }
