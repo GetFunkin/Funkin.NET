cd803551aff19874704a24e04fd5043aa36de8e4
Stevie <xxlennygamerxx@gmail.com>
2021-07-23 17:44:37 -0700

    hmm

--- Funkin.NET/Graphics/Sprites/Characters/FunkinCharacterAnimator.cs
+++ Funkin.NET/Graphics/Sprites/Characters/FunkinCharacterAnimator.cs
@@ -248,11 +248,11 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singRIGHT', 'GF Right Note', 24, false);
 				animation.addByPrefix('singUP', 'GF Up Note', 24, false);
 				animation.addByPrefix('singDOWN', 'GF Down Note', 24, false);
-				animation.addByIndices('sad', 'gf sad', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], "", 24, false);
-				animation.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-				animation.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-				animation.addByIndices('hairBlow', "GF Dancing Beat Hair blowing", [0, 1, 2, 3], "", 24);
-				animation.addByIndices('hairFall', "GF Dancing Beat Hair Landing", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], "", 24, false);
+				animation.addByIndices('sad', 'gf sad', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], '', 24, false);
+				animation.addByIndices('danceLeft', 'GF Dancing Beat', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], '', 24, false);
+				animation.addByIndices('danceRight', 'GF Dancing Beat', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], '', 24, false);
+				animation.addByIndices('hairBlow', 'GF Dancing Beat Hair blowing', [0, 1, 2, 3], '', 24);
+				animation.addByIndices('hairFall', 'GF Dancing Beat Hair Landing', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], '', 24, false);
 				animation.addByPrefix('scared', 'GF FEAR', 24);
 
 				addOffset('cheer');
@@ -260,10 +260,10 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				addOffset('danceLeft', 0, -9);
 				addOffset('danceRight', 0, -9);
 
-				addOffset("singUP", 0, 4);
-				addOffset("singRIGHT", 0, -20);
-				addOffset("singLEFT", 0, -19);
-				addOffset("singDOWN", 0, -20);
+				addOffset('singUP', 0, 4);
+				addOffset('singRIGHT', 0, -20);
+				addOffset('singLEFT', 0, -19);
+				addOffset('singDOWN', 0, -20);
 				addOffset('hairBlow', 45, -8);
 				addOffset('hairFall', 0, -9);
 
@@ -276,9 +276,9 @@ namespace Funkin.NET.Graphics.Sprites.Characters
         public static void RegisterGirlfriendCar(this CharacterDrawable drawable, TextureStore textures)
         {
             /*
-             * animation.addByIndices('singUP', 'GF Dancing Beat Hair blowing CAR', [0], "", 24, false);
-				animation.addByIndices('danceLeft', 'GF Dancing Beat Hair blowing CAR', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-				animation.addByIndices('danceRight', 'GF Dancing Beat Hair blowing CAR', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24,
+             * animation.addByIndices('singUP', 'GF Dancing Beat Hair blowing CAR', [0], '', 24, false);
+				animation.addByIndices('danceLeft', 'GF Dancing Beat Hair blowing CAR', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], '', 24, false);
+				animation.addByIndices('danceRight', 'GF Dancing Beat Hair blowing CAR', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], '', 24,
 					false);
 
 				addOffset('danceLeft', 0);
@@ -291,9 +291,9 @@ namespace Funkin.NET.Graphics.Sprites.Characters
         public static void RegisterGirlfriendPixel(this CharacterDrawable drawable, TextureStore textures)
         {
             /*
-             * animation.addByIndices('singUP', 'GF IDLE', [2], "", 24, false);
-				animation.addByIndices('danceLeft', 'GF IDLE', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
-				animation.addByIndices('danceRight', 'GF IDLE', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+             * animation.addByIndices('singUP', 'GF IDLE', [2], '', 24, false);
+				animation.addByIndices('danceLeft', 'GF IDLE', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], '', 24, false);
+				animation.addByIndices('danceRight', 'GF IDLE', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], '', 24, false);
 
 				addOffset('danceLeft', 0);
 				addOffset('danceRight', 0);
@@ -316,10 +316,10 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singLEFT', 'Dad Sing Note LEFT', 24);
 
 				addOffset('idle');
-				addOffset("singUP", -6, 50);
-				addOffset("singRIGHT", 0, 27);
-				addOffset("singLEFT", -10, 10);
-				addOffset("singDOWN", 0, -30);
+				addOffset('singUP', -6, 50);
+				addOffset('singRIGHT', 0, 27);
+				addOffset('singLEFT', -10, 10);
+				addOffset('singDOWN', 0, -30);
 
 				playAnim('idle');
              */
@@ -332,16 +332,16 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singDOWN', 'spooky DOWN note', 24, false);
 				animation.addByPrefix('singLEFT', 'note sing left', 24, false);
 				animation.addByPrefix('singRIGHT', 'spooky sing right', 24, false);
-				animation.addByIndices('danceLeft', 'spooky dance idle', [0, 2, 6], "", 12, false);
-				animation.addByIndices('danceRight', 'spooky dance idle', [8, 10, 12, 14], "", 12, false);
+				animation.addByIndices('danceLeft', 'spooky dance idle', [0, 2, 6], '', 12, false);
+				animation.addByIndices('danceRight', 'spooky dance idle', [8, 10, 12, 14], '', 12, false);
 
 				addOffset('danceLeft');
 				addOffset('danceRight');
 
-				addOffset("singUP", -20, 26);
-				addOffset("singRIGHT", -130, -14);
-				addOffset("singLEFT", 130, -10);
-				addOffset("singDOWN", -50, -130);
+				addOffset('singUP', -20, 26);
+				addOffset('singRIGHT', -130, -14);
+				addOffset('singLEFT', 130, -10);
+				addOffset('singDOWN', -50, -130);
 
 				playAnim('danceRight');
              */
@@ -350,19 +350,19 @@ namespace Funkin.NET.Graphics.Sprites.Characters
         public static void RegisterMom(this CharacterDrawable drawable, TextureStore textures)
         {
             /*
-             * animation.addByPrefix('idle', "Mom Idle", 24, false);
-				animation.addByPrefix('singUP', "Mom Up Pose", 24, false);
-				animation.addByPrefix('singDOWN', "MOM DOWN POSE", 24, false);
+             * animation.addByPrefix('idle', 'Mom Idle', 24, false);
+				animation.addByPrefix('singUP', 'Mom Up Pose', 24, false);
+				animation.addByPrefix('singDOWN', 'MOM DOWN POSE', 24, false);
 				animation.addByPrefix('singLEFT', 'Mom Left Pose', 24, false);
 				// ANIMATION IS CALLED MOM LEFT POSE BUT ITS FOR THE RIGHT
 				// CUZ DAVE IS DUMB!
 				animation.addByPrefix('singRIGHT', 'Mom Pose Left', 24, false);
 
 				addOffset('idle');
-				addOffset("singUP", 14, 71);
-				addOffset("singRIGHT", 10, -60);
-				addOffset("singLEFT", 250, -23);
-				addOffset("singDOWN", 20, -160);
+				addOffset('singUP', 14, 71);
+				addOffset('singRIGHT', 10, -60);
+				addOffset('singLEFT', 250, -23);
+				addOffset('singDOWN', 20, -160);
 
 				playAnim('idle');
              */
@@ -371,19 +371,19 @@ namespace Funkin.NET.Graphics.Sprites.Characters
         public static void RegisterMomCar(this CharacterDrawable drawable, TextureStore textures)
         {
             /*
-             * animation.addByPrefix('idle', "Mom Idle", 24, false);
-				animation.addByPrefix('singUP', "Mom Up Pose", 24, false);
-				animation.addByPrefix('singDOWN', "MOM DOWN POSE", 24, false);
+             * animation.addByPrefix('idle', 'Mom Idle', 24, false);
+				animation.addByPrefix('singUP', 'Mom Up Pose', 24, false);
+				animation.addByPrefix('singDOWN', 'MOM DOWN POSE', 24, false);
 				animation.addByPrefix('singLEFT', 'Mom Left Pose', 24, false);
 				// ANIMATION IS CALLED MOM LEFT POSE BUT ITS FOR THE RIGHT
 				// CUZ DAVE IS DUMB!
 				animation.addByPrefix('singRIGHT', 'Mom Pose Left', 24, false);
 
 				addOffset('idle');
-				addOffset("singUP", 14, 71);
-				addOffset("singRIGHT", 10, -60);
-				addOffset("singLEFT", 250, -23);
-				addOffset("singDOWN", 20, -160);
+				addOffset('singUP', 14, 71);
+				addOffset('singRIGHT', 10, -60);
+				addOffset('singLEFT', 250, -23);
+				addOffset('singDOWN', 20, -160);
 
 				playAnim('idle');
              */
@@ -399,10 +399,10 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singRIGHT', 'Monster Right note', 24, false);
 
 				addOffset('idle');
-				addOffset("singUP", -20, 50);
-				addOffset("singRIGHT", -51);
-				addOffset("singLEFT", -30);
-				addOffset("singDOWN", -30, -40);
+				addOffset('singUP', -20, 50);
+				addOffset('singRIGHT', -51);
+				addOffset('singLEFT', -30);
+				addOffset('singDOWN', -30, -40);
 				playAnim('idle');
              */
         }
@@ -417,10 +417,10 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singRIGHT', 'Monster Right note', 24, false);
 
 				addOffset('idle');
-				addOffset("singUP", -20, 50);
-				addOffset("singRIGHT", -51);
-				addOffset("singLEFT", -30);
-				addOffset("singDOWN", -40, -94);
+				addOffset('singUP', -20, 50);
+				addOffset('singRIGHT', -51);
+				addOffset('singLEFT', -30);
+				addOffset('singDOWN', -40, -94);
 				playAnim('idle');
              */
         }
@@ -428,7 +428,7 @@ namespace Funkin.NET.Graphics.Sprites.Characters
         public static void RegisterPico(this CharacterDrawable drawable, TextureStore textures)
         {
             /*
-             * animation.addByPrefix('idle', "Pico Idle Dance", 24);
+             * animation.addByPrefix('idle', 'Pico Idle Dance', 24);
 				animation.addByPrefix('singUP', 'pico Up note0', 24, false);
 				animation.addByPrefix('singDOWN', 'Pico Down Note0', 24, false);
 				if (isPlayer)
@@ -451,14 +451,14 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singDOWNmiss', 'Pico Down Note MISS', 24);
 
 				addOffset('idle');
-				addOffset("singUP", -29, 27);
-				addOffset("singRIGHT", -68, -7);
-				addOffset("singLEFT", 65, 9);
-				addOffset("singDOWN", 200, -70);
-				addOffset("singUPmiss", -19, 67);
-				addOffset("singRIGHTmiss", -60, 41);
-				addOffset("singLEFTmiss", 62, 64);
-				addOffset("singDOWNmiss", 210, -28);
+				addOffset('singUP', -29, 27);
+				addOffset('singRIGHT', -68, -7);
+				addOffset('singLEFT', 65, 9);
+				addOffset('singDOWN', 200, -70);
+				addOffset('singUPmiss', -19, 67);
+				addOffset('singRIGHTmiss', -60, 41);
+				addOffset('singLEFTmiss', 62, 64);
+				addOffset('singDOWNmiss', 210, -28);
 
 				playAnim('idle');
 
@@ -480,22 +480,22 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singDOWNmiss', 'BF NOTE DOWN MISS', 24, false);
 				animation.addByPrefix('hey', 'BF HEY', 24, false);
 
-				animation.addByPrefix('firstDeath', "BF dies", 24, false);
-				animation.addByPrefix('deathLoop', "BF Dead Loop", 24, true);
-				animation.addByPrefix('deathConfirm', "BF Dead confirm", 24, false);
+				animation.addByPrefix('firstDeath', 'BF dies', 24, false);
+				animation.addByPrefix('deathLoop', 'BF Dead Loop', 24, true);
+				animation.addByPrefix('deathConfirm', 'BF Dead confirm', 24, false);
 
 				animation.addByPrefix('scared', 'BF idle shaking', 24);
 
 				addOffset('idle', -5);
-				addOffset("singUP", -29, 27);
-				addOffset("singRIGHT", -38, -7);
-				addOffset("singLEFT", 12, -6);
-				addOffset("singDOWN", -10, -50);
-				addOffset("singUPmiss", -29, 27);
-				addOffset("singRIGHTmiss", -30, 21);
-				addOffset("singLEFTmiss", 12, 24);
-				addOffset("singDOWNmiss", -11, -19);
-				addOffset("hey", 7, 4);
+				addOffset('singUP', -29, 27);
+				addOffset('singRIGHT', -38, -7);
+				addOffset('singLEFT', 12, -6);
+				addOffset('singDOWN', -10, -50);
+				addOffset('singUPmiss', -29, 27);
+				addOffset('singRIGHTmiss', -30, 21);
+				addOffset('singLEFTmiss', 12, 24);
+				addOffset('singDOWNmiss', -11, -19);
+				addOffset('hey', 7, 4);
 				addOffset('firstDeath', 37, 11);
 				addOffset('deathLoop', 37, 5);
 				addOffset('deathConfirm', 37, 69);
@@ -522,15 +522,15 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('hey', 'BF HEY', 24, false);
 
 				addOffset('idle', -5);
-				addOffset("singUP", -29, 27);
-				addOffset("singRIGHT", -38, -7);
-				addOffset("singLEFT", 12, -6);
-				addOffset("singDOWN", -10, -50);
-				addOffset("singUPmiss", -29, 27);
-				addOffset("singRIGHTmiss", -30, 21);
-				addOffset("singLEFTmiss", 12, 24);
-				addOffset("singDOWNmiss", -11, -19);
-				addOffset("hey", 7, 4);
+				addOffset('singUP', -29, 27);
+				addOffset('singRIGHT', -38, -7);
+				addOffset('singLEFT', 12, -6);
+				addOffset('singDOWN', -10, -50);
+				addOffset('singUPmiss', -29, 27);
+				addOffset('singRIGHTmiss', -30, 21);
+				addOffset('singLEFTmiss', 12, 24);
+				addOffset('singDOWNmiss', -11, -19);
+				addOffset('hey', 7, 4);
 
 				playAnim('idle');
 
@@ -552,14 +552,14 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singDOWNmiss', 'BF NOTE DOWN MISS', 24, false);
 
 				addOffset('idle', -5);
-				addOffset("singUP", -29, 27);
-				addOffset("singRIGHT", -38, -7);
-				addOffset("singLEFT", 12, -6);
-				addOffset("singDOWN", -10, -50);
-				addOffset("singUPmiss", -29, 27);
-				addOffset("singRIGHTmiss", -30, 21);
-				addOffset("singLEFTmiss", 12, 24);
-				addOffset("singDOWNmiss", -11, -19);
+				addOffset('singUP', -29, 27);
+				addOffset('singRIGHT', -38, -7);
+				addOffset('singLEFT', 12, -6);
+				addOffset('singDOWN', -10, -50);
+				addOffset('singUPmiss', -29, 27);
+				addOffset('singRIGHTmiss', -30, 21);
+				addOffset('singLEFTmiss', 12, 24);
+				addOffset('singDOWNmiss', -11, -19);
 				playAnim('idle');
 
 				flipX = true;
@@ -580,14 +580,14 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singDOWNmiss', 'BF DOWN MISS', 24, false);
 
 				addOffset('idle');
-				addOffset("singUP");
-				addOffset("singRIGHT");
-				addOffset("singLEFT");
-				addOffset("singDOWN");
-				addOffset("singUPmiss");
-				addOffset("singRIGHTmiss");
-				addOffset("singLEFTmiss");
-				addOffset("singDOWNmiss");
+				addOffset('singUP');
+				addOffset('singRIGHT');
+				addOffset('singLEFT');
+				addOffset('singDOWN');
+				addOffset('singUPmiss');
+				addOffset('singRIGHTmiss');
+				addOffset('singLEFTmiss');
+				addOffset('singDOWNmiss');
 
 				setGraphicSize(Std.int(width * 6));
 				updateHitbox();
@@ -606,10 +606,10 @@ namespace Funkin.NET.Graphics.Sprites.Characters
         public static void RegisterBoyfriendPixelDead(this CharacterDrawable drawable, TextureStore textures)
         {
             /*
-             * animation.addByPrefix('singUP', "BF Dies pixel", 24, false);
-				animation.addByPrefix('firstDeath', "BF Dies pixel", 24, false);
-				animation.addByPrefix('deathLoop', "Retry Loop", 24, true);
-				animation.addByPrefix('deathConfirm', "RETRY CONFIRM", 24, false);
+             * animation.addByPrefix('singUP', 'BF Dies pixel', 24, false);
+				animation.addByPrefix('firstDeath', 'BF Dies pixel', 24, false);
+				animation.addByPrefix('deathLoop', 'Retry Loop', 24, true);
+				animation.addByPrefix('deathConfirm', 'RETRY CONFIRM', 24, false);
 				animation.play('firstDeath');
 
 				addOffset('firstDeath');
@@ -634,10 +634,10 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singDOWN', 'SENPAI DOWN NOTE', 24, false);
 
 				addOffset('idle');
-				addOffset("singUP", 5, 37);
-				addOffset("singRIGHT");
-				addOffset("singLEFT", 40);
-				addOffset("singDOWN", 14);
+				addOffset('singUP', 5, 37);
+				addOffset('singRIGHT');
+				addOffset('singLEFT', 40);
+				addOffset('singDOWN', 14);
 
 				playAnim('idle');
 
@@ -658,10 +658,10 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singDOWN', 'Angry Senpai DOWN NOTE', 24, false);
 
 				addOffset('idle');
-				addOffset("singUP", 5, 37);
-				addOffset("singRIGHT");
-				addOffset("singLEFT", 40);
-				addOffset("singDOWN", 14);
+				addOffset('singUP', 5, 37);
+				addOffset('singRIGHT');
+				addOffset('singLEFT', 40);
+				addOffset('singDOWN', 14);
 				playAnim('idle');
 
 				setGraphicSize(Std.int(width * 6));
@@ -674,17 +674,17 @@ namespace Funkin.NET.Graphics.Sprites.Characters
         public static void RegisterSpirit(this CharacterDrawable drawable, TextureStore textures)
         {
             /*
-             * animation.addByPrefix('idle', "idle spirit_", 24, false);
-				animation.addByPrefix('singUP', "up_", 24, false);
-				animation.addByPrefix('singRIGHT', "right_", 24, false);
-				animation.addByPrefix('singLEFT', "left_", 24, false);
-				animation.addByPrefix('singDOWN', "spirit down_", 24, false);
+             * animation.addByPrefix('idle', 'idle spirit_', 24, false);
+				animation.addByPrefix('singUP', 'up_', 24, false);
+				animation.addByPrefix('singRIGHT', 'right_', 24, false);
+				animation.addByPrefix('singLEFT', 'left_', 24, false);
+				animation.addByPrefix('singDOWN', 'spirit down_', 24, false);
 
 				addOffset('idle', -220, -280);
 				addOffset('singUP', -220, -240);
-				addOffset("singRIGHT", -220, -280);
-				addOffset("singLEFT", -200, -280);
-				addOffset("singDOWN", 170, 110);
+				addOffset('singRIGHT', -220, -280);
+				addOffset('singLEFT', -200, -280);
+				addOffset('singDOWN', 170, 110);
 
 				setGraphicSize(Std.int(width * 6));
 				updateHitbox();
@@ -711,14 +711,14 @@ namespace Funkin.NET.Graphics.Sprites.Characters
 				animation.addByPrefix('singRIGHT-alt', 'Parent Right Note Mom', 24, false);
 
 				addOffset('idle');
-				addOffset("singUP", -47, 24);
-				addOffset("singRIGHT", -1, -23);
-				addOffset("singLEFT", -30, 16);
-				addOffset("singDOWN", -31, -29);
-				addOffset("singUP-alt", -47, 24);
-				addOffset("singRIGHT-alt", -1, -24);
-				addOffset("singLEFT-alt", -30, 15);
-				addOffset("singDOWN-alt", -30, -27);
+				addOffset('singUP', -47, 24);
+				addOffset('singRIGHT', -1, -23);
+				addOffset('singLEFT', -30, 16);
+				addOffset('singDOWN', -31, -29);
+				addOffset('singUP-alt', -47, 24);
+				addOffset('singRIGHT-alt', -1, -24);
+				addOffset('singLEFT-alt', -30, 15);
+				addOffset('singDOWN-alt', -30, -27);
 
 				playAnim('idle');
              */
