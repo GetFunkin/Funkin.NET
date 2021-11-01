6d7d60d79c533d1e21595b660a3851fd3433424e
Tomat <xxlennygamerxx@gmail.com>
2021-11-01 12:02:15 -0700

    lol this was on github.dev

--- Funkin.NET.Intermediary/ResourceStores/SparrowAtlasStore.cs
+++ Funkin.NET.Intermediary/ResourceStores/SparrowAtlasStore.cs
@@ -21,68 +21,97 @@ namespace Funkin.NET.Intermediary.ResourceStores
 
         public SparrowAtlasStore(IResourceStore<byte[]>? store = null) : base(store)
         {
+            // We only care about XML files for the sparrow atlas provided by Adobe.
+            // png texture assets are retrieved from paths stored in the XML.
             AddExtension("xml");
         }
 
+        /// <sumary>
+        ///     Retrieve a texture atlas used for retrieving atlas sub-texture frames.
+        /// </summary>
         public Texture GetAtlas(string name)
         {
-            // append expected file extension for standardization
+            // Append the expected file extension for standardization.
             if (!name.EndsWith(".xml"))
                 name += ".xml";
 
-            // if we've already registered all this, no need to do it again
+            // If we've already registered all this, no need to do it again.
+            // Return the cached value.
             if (SparrowAtlases.ContainsKey(name))
                 return SparrowAtlases[name];
 
-            // open a stream to the xml file
+            // Open a usable stream to the XML file.
+            // Disposed of at the end of the method.
             using Stream stream = GetStream(name);
 
             List<SubTexture> subTextures = new();
 
-            // generate mapping of sub-textures from the xml file
+            // Generate a mapping of sub-textures from the XML file.
+            // Uses the stream to get the XML file, populates subTextures.
+            // We also retrieve the imageName.
+            // TODO: SpriteSheetPacker support.
             Necromancer.ReadAsXml(stream, subTextures, out string imageName, out bool successfulRead);
 
-            // find the associated image path
+            // Upon an unsuccessful read attempt: panick.
+            if (!successfulRead)
+                throw new Exception("Attempted to read file with invalid XML data.");
+
+            // Find the associated image path, provided by the XML file.
             string imagePath = PathHelper.SanitizeForResources($"{Path.GetDirectoryName(name) ?? ""}/{imageName}");
 
-            // open a stream to the image
+            // Open a stream to the image.
             using Stream textureStream = GetStream(imagePath);
 
-            // cache the image in memory
+            // Cache the image in a dictionary.
             SparrowAtlases[name] = Texture.FromStream(textureStream);
+
+            // Upscale by 2, unsure of why this is required to properly render textures.
+            // TODO: Figure out why - is this normal/okay??
             SparrowAtlases[name].ScaleAdjust = 2f;
 
             TextureAtlasCache[name] = new Dictionary<string, Texture>();
 
-            if (!successfulRead)
-                throw new Exception("Attempted to read file with invalid XML data.");
-
-            // cache sub-texture data
+            // Store sub-texture mappings.
             SubTextures[name] = subTextures;
 
-            // crop textures based on sub-textures, cache them and make sure we don't create any duplicates
+            // Crop textures based on sub-textures, store them and ensure we don't produce any duplicates.
+            // This helps greatly with performance because this format is gatbage.
             Dictionary<Rectangle, Texture> cropCache = new();
 
+            // Iterate through sub-textures.
             foreach (SubTexture subTexture in SubTextures[name])
             {
+                // Retrieve crop data, we don't care about anything else.
                 subTexture.GetData(out bool _, out Rectangle _, out Rectangle _, out Rectangle crop);
 
-                // add to cache if not already present
+                // Store if not present (based on rectangle cropping!).
+                // Based on rectangle cropping to ensure we don't have duplicate frames.
                 if (!cropCache.ContainsKey(crop))
                 {
-                    cropCache[crop] = SparrowAtlases[name]
-                        .Crop(new osuRect(crop.X, crop.Y, crop.Width, crop.Height));
+                    cropCache[crop] = SparrowAtlases[name].Crop(
+                        new osuRect(
+                            crop.X,
+                            crop.Y,
+                            crop.Width,
+                            crop.Height
+                        )
+                    );
+
+                    // See earlier ScaleAdjust line.
                     cropCache[crop].ScaleAdjust = 2f;
                 }
 
+                // Update values with all cached cropping assets.
                 TextureAtlasCache[name][subTexture.Name] = cropCache[crop];
             }
 
-            // return previously-found texture
+            // Return the atlas.
             return SparrowAtlases[name];
         }
 
-        // return a texture from the atlas
+        /// <summary>
+        ///     Core method for atlas retrieval. Does not handle frames.
+        /// </summary>
         public Texture GetTexture(string atlas, string textureName)
         {
             atlas = atlas.EndsWith(".xml") ? atlas : atlas + ".xml";
@@ -95,6 +124,9 @@ namespace Funkin.NET.Intermediary.ResourceStores
             return TextureAtlasCache[atlas][textureName];
         }
 
+        /// <summary>
+        ///     Get a sub-texture with a given frame from the specified atlas.
+        /// </summary>
         public Texture GetTexture(string atlas, string textureName, int frame) =>
             GetTexture(atlas, textureName + Atlas.FrameAsString(frame));
     }
